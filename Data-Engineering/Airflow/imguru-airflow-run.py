9c1e6db0d56fe1f5e704a19be207da3bf0ed130d
jspark-imguru <jspark@imgr.co.kr>
2023-09-14 03:49:28 +0000

    Update imguru-airflow-run.py

--- Data-Engineering/Airflow/imguru-airflow-run.py
+++ Data-Engineering/Airflow/imguru-airflow-run.py
@@ -43,13 +43,13 @@ t2_cus_orders_test = SparkKubernetesOperator(
     enable_impersonation_from_ldap_user=True    
 ) 
 
-t3_cus_merge_test = SparkKubernetesOperator(
-    task_id='t2-cus-merge-test',
-    namespace="spark",
-    application_file="create-t2-cus-orders-test.yaml",
-    dag=dag,
-    api_group="sparkoperator.hpe.com",
-    enable_impersonation_from_ldap_user=True    
-) 
+#t3_cus_merge_test = SparkKubernetesOperator(
+#    task_id='t2-cus-merge-test',
+#    namespace="spark",
+#    application_file="create-t2-cus-orders-test.yaml",
+#    dag=dag,
+#    api_group="sparkoperator.hpe.com",
+#    enable_impersonation_from_ldap_user=True    
+#) 
 
-t1_cus_info_test >> t2_cus_orders_test >> t3_cus_merge_test
+t1_cus_info_test >> t2_cus_orders_test
